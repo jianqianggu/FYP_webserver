bbe5de640f9cfafdb0176ea8503a62e5a3de995a
Reuel~ <reueltlw@gmail.com>
2023-10-28 04:03:34 +0800

    Beta 1.4 (socket.io installed and integrated)

--- webpage/root/index.js
+++ webpage/root/index.js
@@ -1,11 +1,10 @@
 var express = require('express')
 var app = express();
-const router = express.Router();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const path = require('path');
-
-var server = app.listen(4000, function(){
-  console.log('Server running at http://127.0.0.1:4000/');
-});
 
 // Define the static resource (HTML/CSS/JS/images)
 app.use(express.static(path.join(__dirname, 'public')));             // URL '/' (root) maps to 'public' directory
@@ -16,3 +15,11 @@ var publicPath = path.join(__dirname, 'public');
 app.get('/', function (req, res) {
   res.sendfile(publicPath + '/pages/main.html');
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(4000, '10.245.87.244', () => 
+  console.log('listening on *http://10.245.87.244:4000/')
+);
---
+++ webpage/root/public/js/io.js
@@ -0,0 +1,6 @@
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
